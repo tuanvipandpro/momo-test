6326ed5210393e9ed5c3d5a5415501c76b9dd6f5
TuanLM <tuanvipandpro@gmail.com>
2021-01-17 21:37:39 +0700

    update sha256

--- scripts.js
+++ scripts.js
@@ -5,49 +5,44 @@ const apiEndpoint = 'https://test-payment.momo.vn/gw_payment/transactionProcesso
 
 
 const form = document.getElementById('test')//.addEventListener('submit', e => console.log(e))
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    // console.log(e)
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     // console.log(e)
 
-    const body = {
-        requestType: 'captureMoMoWallet',
-        partnerCode: 'MOMOYYZQ20201025',
-        accessKey: 'LSeeBEDKVSfNuxp0',
-        requestId: 'MOMO0123456789',
-        amount: '500000',
-        orderId: '115484165484514',
-        orderInfo: 'TuanLM',
-        returnUrl: 'http://127.0.0.1:5500/success.html',
-        signature: 'eoEJlBDEl0IQenztGoLUy6U5weESA0iO'
-    }
+//     const body = {
+//         requestType: 'captureMoMoWallet',
+//         partnerCode: 'MOMOYYZQ20201025',
+//         accessKey: 'LSeeBEDKVSfNuxp0',
+//         requestId: 'MOMO0123456789',
+//         amount: '500000',
+//         orderId: '115484165484514',
+//         orderInfo: 'TuanLM',
+//         returnUrl: 'http://127.0.0.1:5500/success.html',
+//         signature: 'eoEJlBDEl0IQenztGoLUy6U5weESA0iO'
+//     }
 
-    axios.post(apiEndpoint, body, {headers: {"Content-Type": "application/x-www-form-urlencoded"}}).then(res => console.log(res))
-})
+//     axios.post(apiEndpoint, body, {headers: {"Content-Type": "application/x-www-form-urlencoded"}}).then(res => console.log(res))
+// })
 
 const payment = () => {
     const body = {
-        partnerCode: 'MOMOYYZQ20201025',
-        accessKey: 'LSeeBEDKVSfNuxp0',
-        requestId: `${(Math.random() * 100000).toFixed(6)}`,
-        amount: '500000',
-        orderId: `${(Math.random() * 100000).toFixed(6)}`,
+        partnerCode: partnerCode,
+        accessKey: accessKey,
+        requestId: `${(Math.random() * 100000).toFixed(0)}`,
+        amount: `${(Math.random() * 100000).toFixed(0)}`,
+        orderId: `${(Math.random() * 100000).toFixed(0)}`,
         orderInfo: 'TuanLM',
-        returnUrl: 'http://127.0.0.1:5500/index.html',
+        returnUrl: 'http://127.0.0.1:5500/success.html',
         notifyUrl: 'https://momo.vn',
-        signature: '7fee16a3958115d80a36f4b56d4e049226e08f58856df4f59f98203ee1a95374',
+        signature: '',
         requestType: 'captureMoMoWallet',
         extraData: "email=abc@gmail.com"
     }
 
-    const signature = CryptoJS.HmacSHA256(`partnerCode=MOMOYYZQ20201025&accessKey=LSeeBEDKVSfNuxp0`, secretKey)
+    body.signature = sha256.hmac(secretKey, `partnerCode=${body.partnerCode}&accessKey=${body.accessKey}&requestId=${body.requestId}&amount=${body.amount}&orderId=${body.orderId}&orderInfo=TuanLM&returnUrl=${body.returnUrl}&notifyUrl=https://momo.vn&extraData=email=abc@gmail.com`)
 
     axios.post(apiEndpoint, body, {headers: {"Content-Type": "application/x-www-form-urlencoded"}}).then(res => {
         console.log(res.data)
-
-        window.open(res.data.payUrl)
+        res.data.payUrl && window.open(res.data.payUrl, '_self')
     })
-
-    // const url = `${apiEndpoint}`
-
-    // window.open()
 }
